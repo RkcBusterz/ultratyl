f1f159a18dbef5d2d05025963e5eebc3671fd333
RkcBusterz <manasichand@outlook.com>
2024-12-26 23:11:16 +0530

    idk why i commited

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const app = express();
 const discordauth = require("./discordauth")
 const port = 3000;
 
-// Serve static files from the 'routes' directory
 app.use(express.static('routes'));
 
-// Route to serve the login.html file
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'routes', 'login.html'));
 });
