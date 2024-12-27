22793e26ef203ca46ccf1bdb02f5ea5ab4322697
RkcBusterz <manasichand@outlook.com>
2024-12-27 13:38:01 +0530

    going to add login logics

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const discordauth = require("./discordauth")
+const discordauth = require("./discordauth");
 const port = 3000;
-
+const sqlite3 = require("sqlite3");
+const database = new sqlite3.Database("logins.db")
 app.use(express.static('routes'));
-
+database.exec(`
+  CREATE TABLE IF NOT EXISTS users(
+  pteroid INT,
+  email TEXT,
+  user TEXT,
+  session_id TEXT
+  )
+  `)
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'routes', 'login.html'));
 });
