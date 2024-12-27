e5b624c8e78ba409d17df9e8755d1f1c2a2aba4d
RkcBusterz <manasichand@outlook.com>
2024-12-27 14:27:49 +0530

    improved the getUser and added addUser function

--- pteroauth.js
+++ pteroauth.js
@@ -1,5 +1,5 @@
 const config = require("./settings.json");
-
+const crypto = require("crypto")
 const getUser = async (email) => {
   const response = await fetch(config.Pterodactyl.panel_url + "/api/application/users", {
     method: "GET",
@@ -11,8 +11,37 @@ const getUser = async (email) => {
   });
 
   const resjson = await response.json();
+  
   const specificUser = resjson.data.find(user => user.attributes.email === email);
-
-  return specificUser.attributes;
+try{
+  return specificUser.attributes;}
+  catch{return undefined}
 };
 
+
+const addUser = async (data) =>{
+if(getUser(data.email) == undefined){
+    
+}else{
+    const response = await fetch(config.Pterodactyl.panel_url+"/api/application/users",{
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${config.Pterodactyl.api_key}`,
+            "Content-Type": "application/json",
+            Accept: "Application/vnd.pterodactyl.v1+json",
+        },
+        body: JSON.stringify({
+            external_id: data.username,
+            username: data.username,
+            email: data.email,
+            first_name: data.username,
+            last_name: "#",
+            password: crypto.randomBytes(16).toString('hex'),
+            root_admin: false,
+            language: "en"
+        })
+    }
+    )}
+}
+
+addUser({email: "hacker6969@rkcmails.net", username: "Hacker6969"})
