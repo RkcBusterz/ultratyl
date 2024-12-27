477bffb41b2ab5fd71a5cdbad61e375b14656d10
RkcBusterz <manasichand@outlook.com>
2024-12-27 14:07:19 +0530

    made getUser function

---
+++ pteroauth.js
@@ -0,0 +1,18 @@
+const config = require("./settings.json");
+
+var getUser = async (email) => {
+  const response = await fetch(config.Pterodactyl.panel_url + "/api/application/users", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${config.Pterodactyl.api_key}`,
+      "Content-Type": "application/json",
+      Accept: "Application/vnd.pterodactyl.v1+json",
+    },
+  });
+
+  const resjson = await response.json();
+  const specificUser = resjson.data.find(user => user.attributes.email === email);
+
+  return specificUser.attributes;
+};
+
